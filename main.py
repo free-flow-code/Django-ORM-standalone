69c41fc6e47cf4bd5c5b8eec08d02943cbdc771d
free-flow-code <wildmuskaria8@gmail.com>
2023-04-11 03:18:42 +0400

    Output to the console of users who are currently in the storage.

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 django.setup()
 
 from datacenter.models import Passcard  # noqa: E402
+from datacenter.models import Visit
 
 if __name__ == '__main__':
     #passcards = Passcard.objects.all()
@@ -13,5 +14,6 @@ if __name__ == '__main__':
     #active_passcards = Passcard.objects.filter(is_active=True)
     #print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
     #print('Активных пропусков: ', len(active_passcards))
-    visits = Passcard.objects.all()
-    print(visits)
+    #visits = Visit.objects.all()
+    in_storage_users = Visit.objects.filter(leaved_at=None)
+    print(in_storage_users)
