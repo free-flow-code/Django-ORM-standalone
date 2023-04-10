90774ac0eda3bf3ff58ce534bfb7b94772c466f0
free-flow-code <wildmuskaria8@gmail.com>
2023-04-11 02:57:24 +0400

    User visits are displayed in the console.

--- main.py
+++ main.py
@@ -8,8 +8,10 @@ django.setup()
 from datacenter.models import Passcard  # noqa: E402
 
 if __name__ == '__main__':
-    passcards = Passcard.objects.all()
-    newline = '\n'
-    active_passcards = Passcard.objects.filter(is_active=True)
-    print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
-    print('Активных пропусков: ', len(active_passcards))
+    #passcards = Passcard.objects.all()
+    #newline = '\n'
+    #active_passcards = Passcard.objects.filter(is_active=True)
+    #print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
+    #print('Активных пропусков: ', len(active_passcards))
+    visits = Passcard.objects.all()
+    print(visits)
