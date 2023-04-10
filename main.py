df4d851e81d7686e2b2e070816d20709fb3396d6
free-flow-code <wildmuskaria8@gmail.com>
2023-04-11 02:25:22 +0400

    Output to the console of all pass cards and only active ones.

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ from datacenter.models import Passcard  # noqa: E402
 if __name__ == '__main__':
     passcards = Passcard.objects.all()
     newline = '\n'
+    active_passcards = []
     for passcard in passcards:
-        print(f'owner_name: {passcard.owner_name}{newline}'
-              f'passcode: {passcard.passcode}{newline}'
-              f'created_at: {passcard.created_at}{newline}'
-              f'is_active: {passcard.is_active}{newline}')
+        if passcard.is_active:
+            active_passcards.append(passcard)
     print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
+    print('Активных пропусков: ', len(active_passcards))
