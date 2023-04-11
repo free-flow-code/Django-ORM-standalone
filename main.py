3b670a65ec0b53206df1426f755d2afc0f7f637a
free-flow-code <wildmuskaria8@gmail.com>
2023-04-11 04:01:14 +0400

    Output to the console the time spent by users in the storage.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
-
+from datetime import datetime
 import django
+from django.utils.timezone import localtime
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 django.setup()
@@ -16,4 +17,9 @@ if __name__ == '__main__':
     #print('Активных пропусков: ', len(active_passcards))
     #visits = Visit.objects.all()
     in_storage_users = Visit.objects.filter(leaved_at=None)
-    print(in_storage_users)
+    for user in in_storage_users:
+        time_now = localtime(timezone=None)
+        timezone_entered_time = localtime(value=user.entered_at, timezone=None)
+        delta = time_now - timezone_entered_time
+        print('Зашел в хранилище, время по Москве:', timezone_entered_time)
+        print('Находится в хранилище:', datetime.fromtimestamp(delta.total_seconds()).strftime('%I:%M:%S'))
