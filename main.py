bcf3ca2e1beeb374557305522fdc63f2428a704a
free-flow-code <wildmuskaria8@gmail.com>
2023-04-11 14:47:33 +0400

    Added functions for obtaining and formatting the time spent by users in the storage.

--- main.py
+++ main.py
@@ -10,6 +10,19 @@ django.setup()
 from datacenter.models import Passcard  # noqa: E402
 from datacenter.models import Visit
 
+
+def get_duration(entered_at):
+    time_now = localtime(timezone=None)
+    delta = time_now - entered_at
+    hours, remainder = divmod(delta.total_seconds(), 3600)
+    minutes, seconds = divmod(remainder, 60)
+    return hours, minutes, seconds
+
+
+def format_duration(hours, minutes, seconds):
+    return f'{int(hours)}:{int(minutes)}:{int(seconds)}'
+
+
 if __name__ == '__main__':
     #passcards = Passcard.objects.all()
     #newline = '\n'
@@ -20,10 +33,7 @@ if __name__ == '__main__':
     in_storage_users = Visit.objects.filter(leaved_at=None)
     for user in in_storage_users:
         print(user.passcard)
-        time_now = localtime(timezone=None)
-        timezone_entered_time = localtime(value=user.entered_at, timezone=None)
-        delta = time_now - timezone_entered_time
-        hours, remainder = divmod(delta.total_seconds(), 3600)
-        minutes, seconds = divmod(remainder, 60)
+        timezone_entered_time = user.entered_at
+        duration = get_duration(timezone_entered_time)
         print('Зашел в хранилище, время по Москве:', timezone_entered_time)
-        print('Находится в хранилище: ', '%s:%s:%s' % (int(hours), int(minutes), int(seconds)))
+        print('Находится в хранилище: ', format_duration(*duration))
