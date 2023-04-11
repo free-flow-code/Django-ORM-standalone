a72d4d603416800b0596929b12ec6150dac6d077
free-flow-code <wildmuskaria8@gmail.com>
2023-04-11 15:04:16 +0400

    The output to the console of active passcards is formatted as a function.

--- main.py
+++ main.py
@@ -33,11 +33,19 @@ def print_users_in_storage():
         print('Находится в хранилище: ', format_duration(*duration))
 
 
+def print_active_passcards():
+    passcards = Passcard.objects.all()
+    newline = '\n'
+    active_passcards = Passcard.objects.filter(is_active=True)
+    for passcard in active_passcards:
+        print(f'owner_name: {passcard.owner_name}{newline}'
+              f'passcode: {passcard.passcode}{newline}'
+              f'created_at: {passcard.created_at}{newline}'
+              f'is_active: {passcard.is_active}{newline}')
+    print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
+    print('Активных пропусков: ', len(active_passcards))
+
+
 if __name__ == '__main__':
-    #passcards = Passcard.objects.all()
-    #newline = '\n'
-    #active_passcards = Passcard.objects.filter(is_active=True)
-    #print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
-    #print('Активных пропусков: ', len(active_passcards))
-    #visits = Visit.objects.all()
+    print_active_passcards()
     print_users_in_storage()
